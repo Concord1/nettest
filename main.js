d94a0165d364c644ce79be2b6b4198cedc04b4b5
Concord1 <78932555+Concord1@users.noreply.github.com>
2023-05-18 14:01:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,25 +5,23 @@ function getAccel(){
   if(mobileUser){
      DeviceMotionEvent.requestPermission().then(response => {
      permis = response;
+   });
    }
-                                                }
-    
-        if (permis == 'granted') {
-            window.addEventListener('deviceorientation',(event) => {
-                x = event.alpha;
-                y = event.beta;
-                z = event.gamma;
+                                            
+    if (permis == 'granted') {
+      window.addEventListener('deviceorientation',(event) => {
+          x = event.alpha;
+          y = event.beta;
+          z = event.gamma;
 
-                var xElement = document.getElementById("x");
-                var yElement = document.getElementById("y");
-                var zElement = document.getElementById("z");
+          var xElement = document.getElementById("x");
+          var yElement = document.getElementById("y");
+          var zElement = document.getElementById("z");
 
-                xElement.textContent = "X: " + x.toFixed(2);
-                yElement.textContent = "Y: " + y.toFixed(2);
-                zElement.textContent = "Z: " + z.toFixed(2);
-          });
-      }
-
-  }
- 
+          xElement.textContent = "X: " + x.toFixed(2);
+          yElement.textContent = "Y: " + y.toFixed(2);
+          zElement.textContent = "Z: " + z.toFixed(2);
+       });
+   }
 }
+ 
