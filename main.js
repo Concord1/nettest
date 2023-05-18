92f1c46a9e4d0230c3979a280731c18657dd1be2
Concord1 <78932555+Concord1@users.noreply.github.com>
2023-05-18 13:33:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-var px = 50; // Position x and y
-var py = 50;
-var vx = 0.0; // Velocity x and y
-var vy = 0.0;
-var updateRate = 1/60; // Sensor refresh rate
-
 function getAccel(){
-  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  // Check for mobile user
+//   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     DeviceMotionEvent.requestPermission().then(response => {
         if (response == 'granted') {
-       // Add a listener to get smartphone orientation 
-           // in the alpha-beta-gamma axes (units in degrees)
             window.addEventListener('deviceorientation',(event) => {
-                // Expose each orientation angle in a more readable way
                 x = event.alpha;
                 y = event.beta;
                 z = event.gamma;
@@ -26,9 +18,10 @@ function getAccel(){
           });
       }
   });
-  }
-  else{
-    document.write("NOOO");
+//   }
+  // Otherwise desktop user
+//   else{
+//     document.write("NOOO");
     
-  }
+//   }
 }
