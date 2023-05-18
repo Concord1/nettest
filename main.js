55cc5ef150c1f1928970bec27453a7fd540f8b04
Concord1 <78932555+Concord1@users.noreply.github.com>
2023-05-18 13:46:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,25 @@
 function getAccel(){
   // Check for mobile user
   var mobileUser = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  var permission = 'granted';
   if(mobileUser){
-    DeviceMotionEvent.requestPermission().then(response => {
-        if (response == 'granted') {
-            window.addEventListener('deviceorientation',(event) => {
-                x = event.alpha;
-                y = event.beta;
-                z = event.gamma;
-
-                var xElement = document.getElementById("x");
-                var yElement = document.getElementById("y");
-                var zElement = document.getElementById("z");
-
-                xElement.textContent = "X: " + x.toFixed(2);
-                yElement.textContent = "Y: " + y.toFixed(2);
-                zElement.textContent = "Z: " + z.toFixed(2);
-          });
-      }
-  });
+    permission = DeviceMotionEvent.requestPermission();
   }
- // Otherwise desktop user
-  else{
-    window.addEventListener('deviceorientation',(event) => {
-                x = event.alpha;
-                y = event.beta;
-                z = event.gamma;
+  if (response == 'granted') {
+      window.addEventListener('deviceorientation',(event) => {
+          x = event.alpha;
+          y = event.beta;
+          z = event.gamma;
 
-                var xElement = document.getElementById("x");
-                var yElement = document.getElementById("y");
-                var zElement = document.getElementById("z");
+          var xElement = document.getElementById("x");
+          var yElement = document.getElementById("y");
+          var zElement = document.getElementById("z");
 
-                xElement.textContent = "X: " + x.toFixed(2);
-                yElement.textContent = "Y: " + y.toFixed(2);
-                zElement.textContent = "Z: " + z.toFixed(2);
-          });
-    
+          xElement.textContent = "X: " + x.toFixed(2);
+          yElement.textContent = "Y: " + y.toFixed(2);
+          zElement.textContent = "Z: " + z.toFixed(2);
+    });
+  }
+  
   }
 }
